71b1d0be3ff2d704159be545bcce23b37203d6df
Siddhant Touti <siddhanttouti@gmail.com>
2019-10-16 21:18:43 +0530

    Added Delivery Executive updates provided by Rushikesh.

---
+++ minip/src/entities/Report.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Report {
+	
+	private String date;
+	private int EmpId;
+	private int count;
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public int getEmpId() {
+		return EmpId;
+	}
+	
+	public void setEmpId(int empId) {
+		EmpId = empId;
+	}
+
+	
+	public int getCount() {
+		return count;
+	}
+
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+
+}
